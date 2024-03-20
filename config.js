a2bcc6b656b0b683ae925d4e39f42c71ece14e7f
DannTeam <110782241+DannOfficial@users.noreply.github.com>
2024-03-20 16:47:10 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,46 @@
+/** 
+ * Big Thanks To
+ * DannTeam
+ * All Team
+ * All Friends
+ * Parents
+ * God
+-------------------
+ * Thanks To
+ * Arifzyn
+ * YanzBot
+ * Danz (me)
+ * Danzz Coding
+ * NowMee (nomisec07)
+*/
+
+// Database Mysql
+global.connection = {
+  host: 'db4free.net',
+  user: 'dannteam',
+  password: 'DaniGTPS01',
+  database: 'dannapi'
+};
+
+// SMTP
+global.email_SMTP = {
+  host: 'smtp.gmail.com',
+  port: 587,
+  secure: false,
+  auth: {
+    user: 'danigtps@gmail.com',
+    pass: 'ecimoyaxlhpbksgq'
+  }
+};
+
+// Limit
+global.limit = {
+  free: 25,
+  premium: 1000
+};
+
+/** 
+ * No encrypt? 10k (https://wa.me/+6283137550315)
+*/
+
+const _0x494b2c=_0x3448;(function(_0x1cff92,_0xaa7e2f){const _0x2a9ccc=_0x3448,_0x511a9b=_0x1cff92();while(!![]){try{const _0x5cd00f=-parseInt(_0x2a9ccc(0x19b))/0x1+parseInt(_0x2a9ccc(0x1a7))/0x2*(parseInt(_0x2a9ccc(0x18d))/0x3)+parseInt(_0x2a9ccc(0x189))/0x4*(-parseInt(_0x2a9ccc(0x193))/0x5)+parseInt(_0x2a9ccc(0x195))/0x6*(parseInt(_0x2a9ccc(0x1ab))/0x7)+-parseInt(_0x2a9ccc(0x19f))/0x8+-parseInt(_0x2a9ccc(0x199))/0x9*(-parseInt(_0x2a9ccc(0x191))/0xa)+-parseInt(_0x2a9ccc(0x1ae))/0xb;if(_0x5cd00f===_0xaa7e2f)break;else _0x511a9b['push'](_0x511a9b['shift']());}catch(_0x4f5949){_0x511a9b['push'](_0x511a9b['shift']());}}}(_0x4998,0x495b7));function _0x4998(){const _0xe6241e=['danigtps@gmail.com','4854cb9fbad67724a2ef9c27a9d1a4e9ded62faa','68CdrWem','cddbceccdc2f1c9d11e4cdd0d2b1d1078e447c43','DannTeam\x20API','https://canvas.dannteam.com','551373YJfFCy','.mp3','remove_bg_API_key','image/jpg','1610SOKxQt','ae128b3094c96bf5fd1a349e7ac03113e21d82c9','15595HEauON','bitly_access_token','1350uOZrPe','cuttly_access_token','2243940c230ad0d748059aee58ddf126b65fd8e7','https://api.dannteam.com/audio/','28233KPjBYm','random','571633GqnMwb','api_canvas','https://api.dannteam.com','https://my.cbox.ws/Dann-API','2571168UKfhnY','https://wa.me/+6283137550315?text=Permisi,\x20saya\x20mau\x20beli\x20user\x20premium\x20paket\x204,\x20dengan\x20harga\x2090k\x20gratis\x201\x20script\x20bot.','port','0f9eb729a7a08ff5e73fe1860c6dc587cc523035','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','length','https://wa.me/+6283137550315?text=Permisi,\x20saya\x20mau\x20heli\x20user\x20premium\x20paket\x205,\x20dengan\x20harga\x20120k\x20gratis\x201\x20script\x20bot.','DannTeam\x20API\x20adalah\x20Restfull\x20API\x27s\x20yang\x20dibuat\x20dengan\x20Node.js','4LxcOKv','https://dannteam.com/thumbnail.jpg','6cfc18e9bfa554714fadc10a1f6aff7555642348','DannTeam','15757XdxqOJ','43f58e789d57247b2cf285d7d24ab755ba383a28','f09ab8db9cf778b37a1cf8bc406eee5063816dec','1458501ygRIpu','secrect_session','100%','Rest-API,\x20Restfull-API,\x20API-Rest,\x20Dann-MD,\x20api,\x20DannTeam','author','e5dee46eb2d69fc9f4b0057266226a52a3555356','971f6c6c2efe6cb5d278b4164acef11c5f21b637','https://telegra.ph/file/b849a052a00dbbfcd53fd.jpg','openai_chatGPT_API_key','a4f429289bf8bf6291be4b1661df57dde5066525','c71b6658a1d271ddaf2a5077de3dcb9d67f68025','7915c671fbd90eca96310e5c9442d761225a1080','password_admin'];_0x4998=function(){return _0xe6241e;};return _0x4998();}const audio_list=['1','2','3','4','5','6','7','8','9','10','11'];shuffle(audio_list);function _0x3448(_0x5c7ad6,_0x34cd61){const _0x499822=_0x4998();return _0x3448=function(_0x34483e,_0x3b807b){_0x34483e=_0x34483e-0x184;let _0x461c43=_0x499822[_0x34483e];return _0x461c43;},_0x3448(_0x5c7ad6,_0x34cd61);}const audio=_0x494b2c(0x198)+audio_list[0x0]+_0x494b2c(0x18e);function shuffle(_0x3ae710){const _0x14694d=_0x494b2c;for(let _0x2ade7b=_0x3ae710[_0x14694d(0x1a4)]-0x1;_0x2ade7b>0x0;_0x2ade7b--){const _0x48e856=Math['floor'](Math[_0x14694d(0x19a)]()*(_0x2ade7b+0x1));[_0x3ae710[_0x2ade7b],_0x3ae710[_0x48e856]]=[_0x3ae710[_0x48e856],_0x3ae710[_0x2ade7b]];}}global['web_settings']={'head':{'title':_0x494b2c(0x18b),'description':_0x494b2c(0x1a6),'author':_0x494b2c(0x1aa),'keywords':_0x494b2c(0x1b1),'site_name':'Restfull\x20API\x27s','domain':_0x494b2c(0x19d),'favicon':_0x494b2c(0x1a8),'image':'https://telegra.ph/file/b849a052a00dbbfcd53fd.jpg','image_type':_0x494b2c(0x190),'image_width':0x190,'image_height':0x12c,'image_alt':_0x494b2c(0x1aa)},'body':{'header':{'firstname':_0x494b2c(0x1aa),'lastname':'API','nav':{'firstname':_0x494b2c(0x1aa),'lastname':'API','cbox':{'url':_0x494b2c(0x19e),'width':_0x494b2c(0x1b0),'height':0x1c2}},'thanks_to':{'name':_0x494b2c(0x1aa)}},'footer':{'name':_0x494b2c(0x1aa)}},'parameters':{'text':_0x494b2c(0x1aa),'image':_0x494b2c(0x1b5)},'pricing':{'plan1':'https://wa.me/+6283137550315?text=Permisi,\x20saya\x20mau\x20beli\x20user\x20premium\x20paket\x201,\x20dengan\x20harga\x2015k','plan2':'https://wa.me/+6283137550315?text=Permisi,\x20saya\x20mau\x20beli\x20user\x20premium\x20paket\x202,\x20dengan\x20harga\x2030k','plan3':'https://wa.me/+6283137550315?text=Permisi,\x20saya\x20mau\x20beli\x20user\x20premium\x20paket\x203,\x20dengan\x20harga\x2060k','plan4':_0x494b2c(0x1a0),'plan5':_0x494b2c(0x1a5)},'contact_support':_0x494b2c(0x187),'random_audio':audio},global[_0x494b2c(0x1a1)]=0xbb8,global[_0x494b2c(0x1af)]=_0x494b2c(0x1aa),global[_0x494b2c(0x186)]='DannKristi',global[_0x494b2c(0x1b2)]='DannTeam',global[_0x494b2c(0x19c)]=_0x494b2c(0x18c),global[_0x494b2c(0x1b6)]=[_0x494b2c(0x1a3)],global[_0x494b2c(0x194)]=[_0x494b2c(0x197),_0x494b2c(0x1a9),_0x494b2c(0x184),_0x494b2c(0x18a),_0x494b2c(0x185),_0x494b2c(0x1b3),_0x494b2c(0x1ad),'964080579f959c0cc3226b4b2053cd6520bb60ad',_0x494b2c(0x1b7),'3d48e2601f25800f375ba388c30266aad54544ae',_0x494b2c(0x188),'d375cf1fafb3dc17e711870524ef4589995c4f69',_0x494b2c(0x1ac),_0x494b2c(0x1b4),_0x494b2c(0x192),'e65f2948f584ffd4c568bf248705eee2714abdd2','08425cf957368db9136484145aa6771e1171e232','dc4bec42a64749b0f23f1a8f525a69184227e301',_0x494b2c(0x1a2),'037c5017712c8f5f154ebbe6f91db1f82793c375'],global[_0x494b2c(0x196)]=['b21fd60e1270f003474834bf1b645888ab8c3'],global[_0x494b2c(0x18f)]=['vhdZxNuajV3g2XGV9eMmQUxu'];
